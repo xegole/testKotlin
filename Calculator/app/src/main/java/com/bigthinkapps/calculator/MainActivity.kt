59f00f4a1d75e470990554cbbaeaf80bd801ceed
Diego Leon <diego.leon@sundevs.com>
2018-09-28 19:37:51 -0500

    added intent action

--- Calculator/app/src/main/java/com/bigthinkapps/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/bigthinkapps/calculator/MainActivity.kt
@@ -1,27 +1,55 @@
 package com.bigthinkapps.calculator
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Toast
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate")
         setContentView(R.layout.activity_main)
-
-        btnLogin.setOnClickListener { onClickButton() }
-
-        labelLogin.setOnClickListener {
-            Toast.makeText(this, "click over label", Toast.LENGTH_SHORT).show()
-            inputMessage.setText("empty value")
-        }
+        btnLogin.setOnClickListener { goToLoginScreen() }
     }
 
-    private fun onClickButton() {
-        val message = inputMessage.text
-        Toast.makeText(this, "this is a message", Toast.LENGTH_SHORT).show()
-        labelLogin.text = message
+    private fun goToLoginScreen() {
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(TAG, "onRestart")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy")
+    }
+
+    companion object {
+        const val TAG = "LogMainActivity"
     }
 }
--- Calculator/app/src/main/java/com/bigthinkapps/calculator/SplashActivity.kt
+++ Calculator/app/src/main/java/com/bigthinkapps/calculator/SplashActivity.kt
@@ -2,20 +2,24 @@ package com.bigthinkapps.calculator
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
+
+    private val handler by lazy { Handler() }
+    private var handlerLate: Handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_splash)
 
-        btnMain.setOnClickListener { goToMainScreen() }
+        handlerLate.postDelayed({ goToMainScreen() }, 3000)
     }
 
     private fun goToMainScreen() {
         val intent = Intent(this, MainActivity::class.java)
         startActivity(intent)
+        finish()
     }
 }
